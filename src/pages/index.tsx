46edc96ad4dfc0e137385e9039aea31371f394af
Shr4528 <Shr4528@gmail.com>
2023-11-06 20:00:31 -0600

    co,,

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <div>Hello World</div>;
+  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
 }
